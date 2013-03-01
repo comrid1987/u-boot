7cc0443047faaec6c74b93c5b96f29dba8850ad1
Matt Porter <mporter@ti.com>
2013-02-28 19:40:50 -0500

    am33xx: refactor emif4/ddr to support multiple EMIF instances
    The AM33xx emif4/ddr support closely matches what is need to support
    TI814x except that TI814x has two EMIF instances. Refactor all the
    emif4 helper calls and the config_ddr() init function to use an
    additional instance number argument.
    
    Signed-off-by: Matt Porter <mporter@ti.com>
    Reviewed-by: Tom Rini <trini@ti.com>
    
    ---
    v4:
    	* fix unused variable warning
    	* fix pcm051 build breakage
    v3:	no change
    v2:
    	* fix bad cast causing ddr.c warnings
    	* Split hardware.h -> hardware_am33xx.h/ti814x.h

---
+++ arch/arm/include/asm/arch-am33xx/hardware_ti814x.h
@@ -0,0 +1,30 @@
+/*
+ * hardware_ti814x.h
+ *
+ * TI814x hardware specific header
+ *
+ * Copyright (C) 2013, Texas Instruments, Incorporated - http://www.ti.com/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR /PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __AM33XX_HARDWARE_TI814X_H
+#define __AM33XX_HARDWARE_TI814X_H
+
+/* VTP Base address */
+#define VTP0_CTRL_ADDR			0x48140E0C
+
+/* DDR Base address */
+#define DDR_PHY_CMD_ADDR		0x47C0C400
+#define DDR_PHY_DATA_ADDR		0x47C0C4C8
+#define DDR_DATA_REGS_NR		4
+
+#endif /* __AM33XX_HARDWARE_TI814X_H */
